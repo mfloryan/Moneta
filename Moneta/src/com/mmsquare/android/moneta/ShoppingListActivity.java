c09b1117993ec6578681dec10f5225763e8cff54
Marcin Floryan <marcin.floryan@gmail.com>
2011-06-21 13:39:59 +0100

    Two Activities

---
+++ Moneta/src/com/mmsquare/android/moneta/ShoppingListActivity.java
@@ -0,0 +1,13 @@
+package com.mmsquare.android.moneta;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import java.util.prefs.BackingStoreException;
+
+public class ShoppingListActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.shopping_list);
+    }
+}
--- Moneta/src/com/mmsquare/android/moneta/StartupActivity.java
+++ Moneta/src/com/mmsquare/android/moneta/StartupActivity.java
@@ -1,7 +1,11 @@
 package com.mmsquare.android.moneta;
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartupActivity extends Activity
 {
@@ -11,5 +15,14 @@ public class StartupActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Button shoppingListButton = (Button) findViewById(R.id_main.shopping_list_button);
+        shoppingListButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(StartupActivity.this, ShoppingListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
