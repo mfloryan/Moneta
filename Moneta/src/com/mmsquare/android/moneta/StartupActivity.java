3298bcb829d24975d0cec3946fcf48439b47911c
Marcin Floryan <marcin.floryan@gmail.com>
2011-06-21 12:54:28 +0100

    Default application structure created by the Android tool

---
+++ Moneta/src/com/mmsquare/android/moneta/StartupActivity.java
@@ -0,0 +1,15 @@
+package com.mmsquare.android.moneta;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartupActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ MonetaTests/src/com/mmsquare/android/moneta/StartupActivityTest.java
@@ -0,0 +1,21 @@
+package com.mmsquare.android.moneta;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * This is a simple framework for a test of an Application.  See
+ * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
+ * how to write and extend Application tests.
+ * <p/>
+ * To run this test, you can type:
+ * adb shell am instrument -w \
+ * -e class com.mmsquare.android.moneta.StartupActivityTest \
+ * com.mmsquare.android.moneta.tests/android.test.InstrumentationTestRunner
+ */
+public class StartupActivityTest extends ActivityInstrumentationTestCase2<StartupActivity> {
+
+    public StartupActivityTest() {
+        super("com.mmsquare.android.moneta", StartupActivity.class);
+    }
+
+}
